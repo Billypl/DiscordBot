bcd14288587eaa6e0a47f72d3c5bee7cd771cd7d
Billypl <62295886+Billypl@users.noreply.github.com>
2022-05-16 14:13:43 +0200

    Configs handlers reorganization

---
+++ TestBot DSharp/Configs/ConfigManager.cs
@@ -0,0 +1,27 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+
+namespace TestBot_DSharp.Configs
+{
+    public static class ConfigManager
+    {
+        public static DiscordConfiguration clientConfig;
+        public static CommandsNextConfiguration commandsConfig;
+        public static InteractivityConfiguration interactivityConfig;
+
+        public static void assignConfigs()
+        {
+            getConfigs();
+            Bot.Client = new DiscordClient(clientConfig);
+            Bot.Commands = Bot.Client.UseCommandsNext(commandsConfig);
+            Bot.Client.UseInteractivity(interactivityConfig);
+        }
+        private static void getConfigs()
+        {
+            ConfigReader.readConfigs();
+        }
+
+    }
+}
---
+++ TestBot DSharp/Configs/ConfigReader.cs
@@ -0,0 +1,58 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace TestBot_DSharp.Configs
+{
+    public static class ConfigReader
+    {
+        public static void readConfigs()
+        {
+            JsonSerializerOptions options = createJsonOptions();
+
+            ConfigManager.clientConfig = readClientConfig(options);
+            ConfigManager.commandsConfig = readCommandsConfig();
+            ConfigManager.interactivityConfig = readInteractivityConfig();
+        }
+        private static JsonSerializerOptions createJsonOptions()
+        {
+            var options = new JsonSerializerOptions();
+            options.Converters.Add(new JsonStringEnumConverter());
+            return options;
+        }
+        private static DiscordConfiguration readClientConfig(JsonSerializerOptions options)
+        {
+            string clientFile = File.ReadAllText("../../Configs/ClientConfig.json");
+            var clientCfg = JsonSerializer.Deserialize<DiscordConfiguration>(clientFile, options);
+            return clientCfg;
+        }
+
+        private static CommandsNextConfiguration readCommandsConfig()
+        {
+            string commandsFile = File.ReadAllText("../../Configs/CommandsConfig.json");
+            var commandsCfg = JsonSerializer.Deserialize<CommandsNextConfiguration>(commandsFile);
+            // prefixes don't deserialize properly, manual deserialization needed
+            commandsCfg.StringPrefixes = getStringPrefixes(commandsFile); 
+            return commandsCfg;
+        }
+        private static IEnumerable<string> getStringPrefixes(string commandsFile)
+        {
+            JsonNode commandNode = JsonNode.Parse(commandsFile);
+            JsonNode prefixNode = commandNode["StringPrefixes"];
+            string prefixes = prefixNode.ToJsonString();
+            return JsonSerializer.Deserialize<IEnumerable<string>>(prefixes);
+        }
+
+        private static InteractivityConfiguration readInteractivityConfig()
+        {
+            string interactivityFile = File.ReadAllText("../../Configs/InteractivityConfig.json");
+            var interactivityCfg = JsonSerializer.Deserialize<InteractivityConfiguration>(interactivityFile);
+            return interactivityCfg;
+        }
+    }
+}
